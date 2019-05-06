faffc01c205c6edc8a01b373a72b4066f3e8882c
Krisli Dimo <krislidimo@gmail.com>
2019-05-06 17:48:35 -0500

    test

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -13,7 +13,7 @@ export default class SavedList extends Component {
         {this.props.list.map(movie => (
           <span className="saved-movie">{movie.title}</span>
         ))}
-        <Link to="/">
+        <Link to="/"> 
           <div className="home-button">Home</div>
         </Link>
       </div>
